bfaa14cdad8fd558bf4c5a05b528062e2f28da97
kris <christopherugwu3@gmail.com>
2022-09-22 05:07:04 -0700

    3-print_alphabt

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ if number < 0 :
 if number > 5 :
     str = f"last_digit of {number} is {last_digit} and its greater than 5"
 elif (last_digit < 6) and (last_digit != 0) :
-    str = f"last_digit of {number} is {last_digit} and its less than 6 and not 0"
+    str = f"last_digit of {number} is {last_digit} and its less than 6 and noy 0"
 elif last_digit == 0 :
     str = f"last_digit of {number} is {last_digit} and is 0"
 print(str)
